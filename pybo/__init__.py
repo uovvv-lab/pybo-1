9ca9fff4d5f036076f608e839c64c9250c68088a
김은솔 <uovvv@naver.com>
2021-05-14 14:59:14 +0900

    저장

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, app
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import config
@@ -9,29 +9,24 @@ migrate = Migrate()
 # --------------------------------------------------------------------------- #
 
 
-def create_app():
-    app = Flask(__name__)
-# --------------------------------- [edit] ---------------------------------- #
-    app.config.from_object(config)
 
-    # ORM
-    db.init_app(app)
-    migrate.init_app(app, db)
 # --------------------------------- [edit] ---------------------------------- #
-    from . import models
+
+# --------------------------------- [edit] ---------------------------------- #
+from . import models
 # --------------------------------------------------------------------------- #
 
-    # 블루프린트
+# 블루프린트
 # --------------------------------------------------------------------------- #
-    from .views import main_views, question_views, answer_views, auth_views, naver_views, movie_views
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(naver_views.bp)
-    app.register_blueprint(movie_views.bp)
+from .views import main_views, question_views, answer_views, auth_views, naver_views, movie_views
+app.register_blueprint(main_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(naver_views.bp)
+app.register_blueprint(movie_views.bp)
 
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+from .filter import format_datetime
+app.jinja_env.filters['datetime'] = format_datetime
 
-    return app
+
